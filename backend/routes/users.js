18efef387e699112032e7e008015e6d7d3df7236
Bob <xbduan@ctrip.com>
2017-10-26 12:41:21 +0800

    update appid

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,9 +8,9 @@ var UserModel = require('../model/UserModel.js');
 
 
 /* GET users listing. */
-router.post('/login', function (req, res, next) {
+router.post('/login', function(req, res, next) {
     function getUserInfo(options, callback) {
-        getOpenid(options, function (err, info) {
+        getOpenid(options, function(err, info) {
             if (err) {
                 callback(err);
             } else {
@@ -21,12 +21,12 @@ router.post('/login', function (req, res, next) {
 
     function getOpenid(options, callback) {
         var userInfo = {};
-        var appid = "wx0075a4b06539973f";
-        var secret = "c41892d77ea156492b70750c43cf3c68";
+        var appid = "wxc4614e1d785b49dd";
+        var secret = "cbc6ef1b21b8a5703128ca38e68fe7ca";
         // https://api.weixin.qq.com/sns/jscode2session?appid=wx0075a4b06539973f&secret=c41892d77ea156492b70750c43cf3c68&js_code=061cBaYG19Pyl60hZrYG1vdfYG1cBaYi&grant_type=authorization_code
 
         var wxUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=" + appid + "&secret=" + secret + "&js_code=" + options.code + "&grant_type=authorization_code"
-        request(wxUrl, function (error, response, body) {
+        request(wxUrl, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var response = JSON.parse(body);
 
@@ -56,7 +56,7 @@ router.post('/login', function (req, res, next) {
     var code = req.body.code;
     getUserInfo({
         code: code
-    }, function (err, info) {
+    }, function(err, info) {
         if (err) {
             res.send({
                 code: -1,
@@ -66,7 +66,7 @@ router.post('/login', function (req, res, next) {
             console.log(info);
             UserModel.findOne({
                 openid: info.openid
-            }, function (err, userInfo) {
+            }, function(err, userInfo) {
                 if (userInfo) {
                     res.send({
                         code: 0,
@@ -80,7 +80,7 @@ router.post('/login', function (req, res, next) {
                         session_key: info.session_key,
                         unionid: info.unionid || "",
                     });
-                    entity.save(function (err, user) {
+                    entity.save(function(err, user) {
                         if (err) {
                             res.send({
                                 code: -1,
@@ -102,7 +102,7 @@ router.post('/login', function (req, res, next) {
 })
 
 
-router.post('/update/:id', function (req, res, next) {
+router.post('/update/:id', function(req, res, next) {
 
 
 });
@@ -113,11 +113,11 @@ router.post('/update/:id', function (req, res, next) {
 
 
 
-router.get('/list/:type', function (req, res, next) {
+router.get('/list/:type', function(req, res, next) {
     if (req.query.username == "admin" && req.query.password == "amdin") {
         UserModel.find({}, {
             type: req.params.type,
-        }, function (err, users) {
+        }, function(err, users) {
             res.send({
                 code: 0,
                 message: "获取成功！",
